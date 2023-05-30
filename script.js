9e166e42d6fb118228b62564e37b2bc216e610bc
Simon Joel <simonjoelwarkentin@proton.me>
2023-05-30 15:20:32 +0000

    added theme switch button

--- script.js
+++ script.js
@@ -10,3 +10,29 @@ function hideContent(id) {
   var content = document.getElementById("content" + id);
   content.style.opacity = "0";
 }
+
+
+
+
+//THEME SWITCH BUTTON
+
+// Get the necessary elements
+const themeToggle = document.getElementById('theme-toggle');
+const themeStyle = document.getElementById('theme-style');
+const darkTheme = document.getElementById('dark-theme');
+
+// Function to toggle the themes
+function toggleTheme() {
+  if (themeStyle.disabled) {
+    // Enable dark theme
+    themeStyle.disabled = false;
+    darkTheme.disabled = true;
+  } else {
+    // Enable light theme
+    themeStyle.disabled = true;
+    darkTheme.disabled = false;
+  }
+}
+
+// Add event listener to the toggle button
+themeToggle.addEventListener('click', toggleTheme);
