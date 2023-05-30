73f2bd74a968a9a67e173ba77276aea0c1d9949a
Simon Joel <simonjoelwarkentin@proton.me>
2023-05-30 15:37:32 +0000

    made it work for the iframe

--- script.js
+++ script.js
@@ -20,6 +20,7 @@ function hideContent(id) {
 const themeToggle = document.getElementById('theme-toggle');
 const themeStyle = document.getElementById('theme-style');
 const darkTheme = document.getElementById('dark-theme');
+const iframe = document.getElementById('my-iframe');
 
 // Function to toggle the themes
 function toggleTheme() {
@@ -27,10 +28,14 @@ function toggleTheme() {
     // Enable dark theme
     themeStyle.disabled = false;
     darkTheme.disabled = true;
+    iframe.contentWindow.document.getElementById('theme-style').disabled = false;
+    iframe.contentWindow.document.getElementById('dark-theme').disabled = true;
   } else {
     // Enable light theme
     themeStyle.disabled = true;
     darkTheme.disabled = false;
+    iframe.contentWindow.document.getElementById('theme-style').disabled = true;
+    iframe.contentWindow.document.getElementById('dark-theme').disabled = false;
   }
 }
 
