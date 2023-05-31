ecec11399194ef6fc4c5745a7b32b61e53d74e3a
Simon Joel Warkentin <119256717+SimonJoelWarkentin@users.noreply.github.com>
2023-05-31 23:22:28 +0100

    added bar for buttons (worked hard on this one)

--- script.js
+++ script.js
@@ -1,21 +1,7 @@
 
-//THIS (KIND OF) WORKS.
-//It has some obvious issues, but I AM NOT DELETING IT UNTIL I GET THE REST TO BE FUNCTIONAL.
-
-function showContent(id) {
-  var content = document.getElementById("content" + id);
-  content.style.opacity = "1";
-}
-function hideContent(id) {
-  var content = document.getElementById("content" + id);
-  content.style.opacity = "0";
-}
-
-
 
 
 //THEME SWITCH BUTTON
-
 // Get the necessary elements
 const themeToggle = document.getElementById('theme-toggle');
 const themeStyle = document.getElementById('theme-style');
